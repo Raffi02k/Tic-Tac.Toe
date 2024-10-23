6f0858f5c2e9feb77752ddc272fe4a7b90693985
raffimedz <raffi.medzadaghlian@iths.se>
2024-10-24 00:41:43 +0200

    the code is looking clean..

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.sistalab3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires javafx.media;
 
 
     opens com.example.sistalab3 to javafx.fxml;
